e180f5bf1f01e6da03361237b76dc9af30d5aead
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-10-31 15:31:13 +0100

    Worked on i18n which stands for internalization

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Force locale with URL parameter.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+def get_locale():
+    """
+     detect if the incoming request contains locale
+    argument and if its value is a supported locale, return it
+    """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
+    """
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
